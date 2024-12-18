006b5b01542fd860feee99d4792ab818cb9cfab2
Mohammadul Kader <mksoikotbhuiyan117@gmail.com>
2024-12-18 12:33:04 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/',[AuthController::class, 'login']);
+Route::post('/',[AuthController::class, 'auth_login']);
+
+Route::get('logout',[AuthController::class, 'logout']);
+
+Route::group(['middleware' => 'useradmin'], function () {
+    Route::get('panel/dashboard',[DashboardController::class, 'dashboard']);
+});
+
